265cad461458e28cdcc201570bd6aec8ce9a29dd
nosthrillz <ilie06@gmail.com>
2021-09-28 11:25:11 +0200

    persist store to localStorage

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 // Components
 import App from "./App";
 // Libs/Styles
-import { store } from "./store/index";
+import store from "./store/index";
 import "./styles/globals.scss";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,27 @@
 // Functionality
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 // Libs/styles
 import cartReducer from "../features/cartReducer";
 import productReducer from "../features/productReducer";
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    products: productReducer,
-  },
+const reducers = combineReducers({
+  cart: cartReducer,
+  products: productReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export default store;
